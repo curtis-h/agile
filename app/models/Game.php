f65aed4ae608d9d71ebdd59e1139964abccb5a3e
Mike Elsmore <mike@elsmore.me>
2014-05-17 19:50:32 +0100

    Add game object

---
+++ app/models/Game.php
@@ -0,0 +1,20 @@
+<?php
+
+class Game extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'game';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $primaryKey = 'id';
+
+
+}
